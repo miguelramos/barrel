f53993388c6179bce4c8467104f2c37aef03e123
Miguel Ramos <miguelramos@nqda.pt>
2021-02-04 02:47:36 +0000

    feat: env config and struct

--- config/conf.go
+++ config/conf.go
@@ -1,26 +1,30 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
 
+// EnvironmentConfig flags
 type EnvironmentConfig struct {
-	BarrelPort              string
-	BarrelHost              string
-	BarrelDatabaseURL       string
-	BarrelDatabaseNamespace string
-	BarrelJWTSecret         string
-	BarrelBaseURL           string
-	BarrelGotrueURL         string
-	MinioAccessKey          string
-	MinioSecretKey          string
-	MinioPort               string
-	MinioURL                string
+	BarrelPort              string `env:"BARREL_PORT" mapstructure:"port"`
+	BarrelHost              string `env:"BARREL_HOST" mapstructure:"host"`
+	BarrelDatabaseURL       string `env:"BARREL_DATABASE_URL" mapstructure:"database_url"`
+	BarrelDatabaseNamespace string `env:"BARREL_DATABASE_NAMESPACE" mapstructure:"database_namespace"`
+	BarrelJWTSecret         string `env:"BARREL_JWT_SECRET" mapstructure:"jwt_secret"`
+	BarrelBaseURL           string `env:"BARREL_BASE_URL" mapstructure:"base_url"`
+	BarrelGotrueURL         string `env:"BARREL_GOTRUE_URL" mapstructure:"gotrue_url"`
+	BarrelMinioAccessKey    string `env:"BARREL_MINIO_ACCESS_KEY" mapstructure:"minio_access_key"`
+	BarrelMinioSecretKey    string `env:"BARREL_MINIO_SECRET_KEY" mapstructure:"minio_secret_key"`
+	BarrelMinioPort         string `env:"BARREL_MINIO_PORT" mapstructure:"minio_port"`
+	BarrelMinioURL          string `env:"BARREL_MINIO_URL" mapstructure:"minio_url"`
 }
 
+// LoadEnvironmentConfig load config from env
 func LoadEnvironmentConfig() *EnvironmentConfig {
 	viper := viper.New()
 
 	loadEnv(viper)
-	loadDefault(viper)
 
 	envConfig := new(EnvironmentConfig)
 
@@ -30,19 +34,35 @@ func LoadEnvironmentConfig() *EnvironmentConfig {
 }
 
 func loadDefault(viper *viper.Viper) {
-	viper.SetDefault("MINIO_PORT", "9876")
-	viper.SetDefault("MINIO_URL", "localhost:9876")
-	viper.SetDefault("MINION_PORT", "9078")
-	viper.SetDefault("MINION_HOST", "localhost")
-	viper.SetDefault("MINION_DATABASE_NAMESPACE", "storage")
-	viper.SetDefault("MINION_BASE_URL", "http://localhost:9078")
-	viper.SetDefault("MINION_GOTRUE_URL", "http://localhost:9999")
+	viper.SetDefault("BARREL_MINIO_PORT", "9876")
+	viper.SetDefault("BARREL_MINIO_URL", "localhost:9876")
+	viper.SetDefault("BARREL_PORT", "9078")
+	viper.SetDefault("BARREL_HOST", "localhost")
+	viper.SetDefault("BARREL_DATABASE_NAMESPACE", "storage")
+	viper.SetDefault("BARREL_BASE_URL", "http://localhost:9078")
+	viper.SetDefault("BARREL_GOTRUE_URL", "http://localhost:9999")
 }
 
 func loadEnv(viper *viper.Viper) {
-	viper.AddConfigPath("./")
-	viper.SetConfigFile(".env")
+	viper.SetEnvPrefix("barrel")
+	viper.SetConfigFile("../.env")
+
 	viper.AutomaticEnv()
-	viper.ReadInConfig()
-	viper.WatchConfig()
+	viper.BindEnv("MINIO_PORT")
+	viper.BindEnv("MINIO_URL")
+	viper.BindEnv("MINIO_ACCESS_KEY")
+	viper.BindEnv("MINIO_SECRET_KEY")
+	viper.BindEnv("PORT")
+	viper.BindEnv("HOST")
+	viper.BindEnv("DATABASE_URL")
+	viper.BindEnv("DATABASE_NAMESPACE")
+	viper.BindEnv("JWT_SECRET")
+	viper.BindEnv("BASE_URL")
+	viper.BindEnv("GOTRUE_URL")
+
+	loadDefault(viper)
+
+	if err := viper.ReadInConfig(); err != nil {
+		logrus.Info("Env file not present")
+	}
 }
